080062480d34f0f2154a589440a9a5b177e2defd
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2021-06-07 09:21:22 +0200

    Update rnn.py

--- pipeline/neural_translation/multilingual_rnns/rnn.py
+++ pipeline/neural_translation/multilingual_rnns/rnn.py
@@ -1,7 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# This code is in the process of being integrated in fairseq. 
 
 from typing import Dict, List, Optional, Tuple, Union
 from dataclasses import dataclass, field
